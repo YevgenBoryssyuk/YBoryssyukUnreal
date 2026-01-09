ad26030eee0ebf4c974e5e631b65a63d8cd3ee27
YevgenBoryssyuk <144417199+YevgenBoryssyuk@users.noreply.github.com>
2026-01-09 03:00:03 +0200

    Task 16

---
+++ Task16YB/Task16YB.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string Name;
+    int Health;
+    std::string CharacterClass;
+
+    std::cout << "Hi player! To create your character you need to write your name, write your health from 1 to 100 and choose class from (warrior, mage, archer):\n";
+    std::cin >> Name >> Health >> CharacterClass;
+    std::cout << "Hi " << Name << ", your current health is " << Health << " and your class is " << CharacterClass << "!\n";
+    
+    if (Health == 100) {
+        std::cout << "Your health is max!\n";
+    }
+    else {
+        std::cout << "Your health is " << Health << "\n";
+    }
+    if (CharacterClass == "warrior" || CharacterClass == "mage" || CharacterClass == "archer") {
+        std::cout << "You choose right class!\n";
+    }
+    else {
+        std::cout << "You choose wrong class!\n";
+    }
+}
