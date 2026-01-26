601895815afcb2d8df2ef99d67e8eef98bfded5d
YevgenBoryssyuk <144417199+YevgenBoryssyuk@users.noreply.github.com>
2026-01-26 04:17:34 +0200

    Task 19: Character cordinates, use 4 commands

---
+++ Task19YB/Task19YB.cpp
@@ -0,0 +1,50 @@
+﻿// Команди руху: right, left, forward, backward (ходять на 1 клітинку)
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+int main()
+{
+    int commandCount;
+    int i = 0;
+
+    std::vector<std::string> commands;
+
+    std::cout << "How many commands? ";
+    std::cin >> commandCount;
+
+    while (i < commandCount) {
+        std::string command;
+        std::cin >> command;
+        commands.push_back(command);
+        i++;
+    }
+
+    int x, y;
+    std::cout << "Enter start position (x y); ";
+    std::cin >> x >> y;
+    
+    std::cout << "Commands stored:\n";
+    
+    for (std::string& c : commands) {
+        for (char& ch : c) {
+            ch = std::tolower(ch);
+        }
+
+        if (c == "right") {
+            x += 1;
+        }
+        else if (c == "left") {
+            x -= 1;
+        }
+        else if (c == "forward") {
+            y += 1;
+        }
+        else if (c == "backward") {
+            y -= 1;
+        }
+    }
+
+    std::cout << "Final position: (" << x << ", " << y << ")\n";
+}
