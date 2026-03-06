c390b30a5ba9fcc7743fc58f2585ae9e50371bf7
Yevgen Boryssyuk <borisyuk.evgenij@gmail.com>
2026-03-06 02:39:03 +0200

    Task26 - Creating an Unreal Engine C++ project

---
+++ Source/Task26YB/Task26YB.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Task26YB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Task26YB, "Task26YB" );
